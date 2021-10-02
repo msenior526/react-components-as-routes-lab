8c57661411ef8217440e004cf6f5bc285b2fc978
msenior526 <msenior526@gmail.com>
2021-10-02 15:36:36 -0400

    commit tree 9f750692c16779571b4c021d9382c326fe49c8e3

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -4,7 +4,16 @@ import { directors } from '../data';
 const Directors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Directors Page</h1>
+      {directors.map((director, idx) => (
+        <div key={idx}>
+          <h3>{director.name}</h3>
+          <ul>{director.movies.map((movie, idx) => (
+            <li key={idx}>{movie}</li>
+          ))}
+          </ul>
+        </div>
+      ))}
     </div>
   );
 }
